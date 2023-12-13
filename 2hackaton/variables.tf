c741bcb0d49b2d070efea71fb65fc0d2e411054f
juniorpaiva <edvaldojuniorinfo@gmail.com>
2023-12-13 00:25:41 -0300

    version1.1

--- 2hackaton/variables.tf
+++ 2hackaton/variables.tf
@@ -1,14 +1,14 @@
 variable "access_key" {
     description = "Valor da access_key"
     type = string
-    default = "AKIASDKUF7RRLRH7VU7Q"
-    sensitive = true  
+    sensitive = true
+    default = "AKIASDKUF7RRLRH7VU7Q"  
 }
 
 variable "secret_key" {
     description = "Valor da secret_key"
     type = string
-    default = "kuwLFf9phWcV8CPdI3UKkQjtQUohErdXA/pC3/wP"
     sensitive = true
+    default = "kuwLFf9phWcV8CPdI3UKkQjtQUohErdXA/pC3/wP"
 }
 
